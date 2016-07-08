d0e3b697d99d152ba880f2447695a39ec20e34e6
Brett Lischalk <blischalk@opinionlab.com>
2016-07-08 15:13:41 -0500

    Working. Completes in acceptable amounts of time.

--- src/printable_helper/core.clj
+++ src/printable_helper/core.clj
@@ -5,21 +5,6 @@
   (map int "%_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"))
 
 
-(def byte-perms
-  (for [byte-a printable-bytes
-        byte-b printable-bytes
-        byte-c printable-bytes
-        byte-d printable-bytes]
-    (bit-or byte-a
-            (bit-shift-left byte-b 8)
-            (bit-shift-left byte-c 16)
-            (bit-shift-left byte-d 24))))
-
-
-(defn winning-subtraction-set? [start end words]
-  (= end (bit-and (apply - start words) 0xffffffff)))
-
-
 (defn n-length-perms [byte-perms max-subtractions]
   (if (zero? max-subtractions) '(())
       (mapcat (fn [c]
@@ -35,22 +20,64 @@
                 (all-perms byte-perms (dec max-subtractions)))))
 
 
-(defn find-n-subtractions [start end permutations]
-  (first (filter (partial winning-subtraction-set? start end) permutations)))
+(def byte-combinations
+  (lazy-cat (all-perms printable-bytes 1)
+            (all-perms printable-bytes 2)
+            (all-perms printable-bytes 3)
+            (all-perms printable-bytes 4)))
 
 
-(defn print-subtractions [[start end max-subtractions]]
+(defn calc-words [[start-bytes end-bytes] carry results]
+  (let [sb (first start-bytes) eb (first end-bytes)]
+    (if (and sb eb)
+      (let [match (first (filter #(= sb (bit-and (apply + carry eb %) 0x000000ff)) byte-combinations))
+            new-carry (bit-shift-right (bit-and (apply + carry eb match) 0x0000ff00) 8)
+            new-results (conj results match)]
+        (recur [(rest start-bytes) (rest end-bytes)] new-carry new-results))
+      results)))
+
+
+(defn get-bytes [start end]
+  (let [sb1 (bit-and start 0x000000ff)
+        sb2 (bit-shift-right (bit-and start 0x0000ff00) 8)
+        sb3 (bit-shift-right (bit-and start 0x00ff0000) 16)
+        sb4 (bit-shift-right (bit-and start 0xff000000) 24)
+        eb1 (bit-and end 0x000000ff)
+        eb2 (bit-shift-right (bit-and end 0x0000ff00) 8)
+        eb3 (bit-shift-right (bit-and end 0x00ff0000) 16)
+        eb4 (bit-shift-right (bit-and end 0xff000000) 24)]
+    [[sb1 sb2 sb3 sb4] [eb1 eb2 eb3 eb4]]))
+
+(def byte-offsets [0 8 16 24])
+
+(defn format-words [words]
+  (map #(format "0x%08x" %)
+       (remove zero?
+               (reduce
+                (fn [coll itm]
+                  (conj coll (apply bit-or
+                                    (map-indexed
+                                     (fn [idx i]
+                                       (bit-shift-left
+                                        i
+                                        (get byte-offsets idx)))
+                                     itm))))
+                []
+                (for [n (range 0 4)]
+                  (map #(nth % n 0) words))))))
+
+
+(defn print-subtractions' [[start end]]
   (println "Calculating required subtractions...")
   (println "Starting address: " start)
-  (doseq [subtraction (find-n-subtractions
-                       (read-string start)
-                       (read-string end)
-                       (all-perms byte-perms (read-string max-subtractions)))]
-    (println (format "                 - 0x%08x" subtraction)))
+  (time (doseq [word (-> (get-bytes (read-string start) (read-string end))
+                         (calc-words 0 [])
+                         (format-words))]
+          (println word)))
   (println "Ending address:   " end))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (print-subtractions args))
+  (print-subtractions' args))
