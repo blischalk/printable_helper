dc53fd357f0b08a180a4a53bb306ea06f2f16e6d
Brett Lischalk <blischalk@opinionlab.com>
2016-07-08 16:03:34 -0500

    Adding documentation

--- src/printable_helper/core.clj
+++ src/printable_helper/core.clj
@@ -2,42 +2,72 @@
   (:gen-class))
 
 (def printable-bytes
+  "All the printable bytes we can use in a memory address."
   (map int "%_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"))
 
 
-(defn n-length-perms [byte-perms max-subtractions]
-  (if (zero? max-subtractions) '(())
+(def byte-offsets
+  "Byte offsets useful for shifting."
+  [0 8 16 24])
+
+
+(defn n-length-perms
+  "Calculates all the permutions of items including n items."
+  [items n]
+  (if (zero? n) '(())
       (mapcat (fn [c]
                 (map #(cons c %)
-                     (n-length-perms byte-perms
-                                     (dec max-subtractions))))
-              byte-perms)))
+                     (n-length-perms items (dec n))))
+              items)))
 
 
-(defn all-perms [byte-perms max-subtractions]
-  (if (zero? max-subtractions) '()
-      (lazy-cat (n-length-perms byte-perms max-subtractions)
-                (all-perms byte-perms (dec max-subtractions)))))
+(defn all-perms
+  "Calculates all the permutations of items including
+  1 up to and includig n items."
+  [items n]
+  (if (zero? n) '()
+      (lazy-cat (all-perms items (dec n))
+                (n-length-perms items n))))
 
 
 (def byte-combinations
-  (lazy-cat (all-perms printable-bytes 1)
-            (all-perms printable-bytes 2)
-            (all-perms printable-bytes 3)
-            (all-perms printable-bytes 4)))
+  "lazy sequency of all permutations of 1 - 4 byte
+  combinations of the allowed printable bytes."
+  (all-perms printable-bytes 4))
 
 
-(defn calc-words [[start-bytes end-bytes] carry results]
+(defn calc-words
+  "Recursively builds reversed memory addresses as collections  of bytes e.g
+  lowest order byte first in each collection. A column of bytes, e.g the byte in
+  the same position of each collection includes the required bytes
+  that when subtracted from a starting byte will achieve the desired difference.
+
+  Each collection is a reversed word.
+  Each position within a collection will end up subtracted from the same
+  position in the next collection the same way you would do normal subtraction,
+  starting with the lowest order term of each number, subtracting, and
+  working your way towards the highest order terms.
+  "
+  [[start-bytes end-bytes] carry results]
   (let [sb (first start-bytes) eb (first end-bytes)]
     (if (and sb eb)
-      (let [match (first (filter #(= sb (bit-and (apply + carry eb %) 0x000000ff)) byte-combinations))
-            new-carry (bit-shift-right (bit-and (apply + carry eb match) 0x0000ff00) 8)
+      (let [match (first (filter #(= sb (bit-and (apply + carry eb %)
+                                                 0x000000ff))
+                                 byte-combinations))
+            new-carry (bit-shift-right (bit-and (apply + carry eb match)
+                                                0x0000ff00)
+                                       8)
             new-results (conj results match)]
         (recur [(rest start-bytes) (rest end-bytes)] new-carry new-results))
       results)))
 
 
-(defn get-bytes [start end]
+(defn get-bytes
+  "Takes a starting and ending 4 byte word memory address
+  and breaks out the individual bytes of each address into
+  a collection of starting bytes and ending bytes. The bytes
+  are ordered lowest to highest order."
+  [start end]
   (let [sb1 (bit-and start 0x000000ff)
         sb2 (bit-shift-right (bit-and start 0x0000ff00) 8)
         sb3 (bit-shift-right (bit-and start 0x00ff0000) 16)
@@ -48,9 +78,12 @@
         eb4 (bit-shift-right (bit-and end 0xff000000) 24)]
     [[sb1 sb2 sb3 sb4] [eb1 eb2 eb3 eb4]]))
 
-(def byte-offsets [0 8 16 24])
 
-(defn format-words [words]
+(defn format-words
+  "Pad's words with 0's and turns the reversed collections of
+   bytes from calc-words into actual memory addresses. Proceeds to
+   format each word as a 4 byte string for printing."
+  [words]
   (map #(format "0x%08x" %)
        (remove zero?
                (reduce
